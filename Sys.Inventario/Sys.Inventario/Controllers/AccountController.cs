813f64ff1c9bf35d45a14021b6674c4717268cc2
neyder <neydermurillo@hotmail.com>
2020-10-14 15:35:39 -0500

    creacion de controlador  para  la verificacion el usuario y la iniciacion de seccion

---
+++ Sys.Inventario/Sys.Inventario/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+//este controlador va averificar si esta el usuario 
+namespace Sys.Inventario.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index(string email, string password)
+        {
+            return View();
+        }
+
+    }
+}
